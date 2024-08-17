09ba945deb715df881a0606819a3817def9e7da5
nekefer <kloutsedaniel1@gmail.com>
2024-08-16 22:08:40 -0500

    annimation

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('share')
+const popup = document.getElementById('popup')
+
+button.addEventListener('click', () => {
+    popup.classList.toggle('active')
+    console.log('clicked')
+    console.log(popup.classList)
+
+})
