f3fedfe446b9948f0b27b24b536960483236a2be
nekefer <kloutsedaniel1@gmail.com>
2024-08-23 17:18:48 -0500

    responsive

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const popup = document.getElementById('popup')
 
 button.addEventListener('click', () => {
     popup.classList.toggle('active')
+    button.classList.toggle('active')
     console.log('clicked')
     console.log(popup.classList)
 
